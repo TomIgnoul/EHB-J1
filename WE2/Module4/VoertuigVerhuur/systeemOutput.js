9d99f8b95d48494edb18953edb73495e672eda15
Tom Ignoul <tom.ignoul@student.ehb.be>
2025-08-08 23:09:25 +0200

    started the table

--- WE2/Module4/VoertuigVerhuur/systeemOutput.js
+++ WE2/Module4/VoertuigVerhuur/systeemOutput.js
@@ -1,10 +1,11 @@
 import { Voertuig, Auto, Motor } from "./systeem.js";
-console.log("JS loaded!");
+
 const Voertuigen = {
   Toyota: new Auto("Toyota", "Yaris", 2020, 50),
   BMW: new Auto("BMW", "3 series", 2021, 80),
   BYD: new Auto("BYD", "Dolphine", 2022, 70),
 };
+
 console.log(Voertuigen);
 document.addEventListener("DOMContentLoaded", () => {
   var selectCar = document.getElementById("selectCar");
@@ -15,4 +16,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const voertuig = Voertuigen[name];
     console.log(voertuig.verhuur());
   });
+});
+
+let voertuigProperties = ["Merk", "Model", "Bouwjaar", "VerhuurPrijs"];
+let carDisplay = document.getElementById("carDisplay");
+let carDisplayHead = carDisplay.createTHead();
+let carDisplayRow = carDisplayHead.insertRow();
+
+voertuigProperties.forEach((element) => {
+  let th = document.createElement("th");
+  th.textContent = element;
+  carDisplayRow.appendChild(th);
 });
