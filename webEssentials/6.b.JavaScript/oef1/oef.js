4117b1c07a552f168b2c1ec892f9c8e907167269
Tom Ignoul <tom.ignoul@student.ehb.be>
2025-08-29 10:22:21 +0200

    JavaScript: Oef1

---
+++ webEssentials/6.b.JavaScript/oef1/oef.js
@@ -0,0 +1,9 @@
+let el = document.getElementById("titel2");
+console.log(el);
+let lijst = document.querySelectorAll("p");
+for (let i = 0; i < lijst.length; i++) {
+  console.log(lijst[i]);
+}
+
+let speciaal = document.querySelector(".speciaal");
+console.log(speciaal.textContent);
